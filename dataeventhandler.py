04c9dfb8db332b1d5276cc83456660dc6877ec5b
Dami500 <damilareajayiobe0@gmail.com>
2024-09-28 15:39:51 -0400

    Update dataeventhandler.py

--- dataeventhandler.py
+++ dataeventhandler.py
@@ -6,8 +6,8 @@ import queue
 import warnings
 warnings.filterwarnings("ignore")
 db_host = 'localhost'
-db_user = 'sec_user'
-db_pass = 'Damilare20$'
+db_user = 'user'
+db_pass = 'password'
 db_name = 'securities_master'
 con = msc.connect(host=db_host, user=db_user, password=db_pass, db=db_name)
 
